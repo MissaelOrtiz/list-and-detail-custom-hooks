d5df9c26541011c797530caab0930905d9321fa1
Missael Ortiz <ortiz.missael80@gmail.com>
2021-09-15 16:28:43 -0700

    added useDetailEvents hook to useEvents hooks

--- src/hooks/useEvents.js
+++ src/hooks/useEvents.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { fetchApi } from '../services/animalCrossingApi';
+import { fetchApi, fetchVillager } from '../services/animalCrossingApi';
 
 export const useListEvents = () => {
   const [loading, setLoading] = useState(true);
@@ -13,4 +13,18 @@ export const useListEvents = () => {
   }, []);
 
   return { loading, villagers };
+};
+
+export const useDetailEvents = () => {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [villager, setVillager] = useState({});
+  
+  useEffect(() => {
+    fetchVillager(id)
+      .then((villager) => setVillager(villager))
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  return { loading, villager }; 
 };
